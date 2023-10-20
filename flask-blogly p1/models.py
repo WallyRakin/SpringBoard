f75b2688f3ee9052556f4e88bad874fd500e6929
WallyRakin <83788022+WallyRakin@users.noreply.github.com>
2023-10-20 02:16:50 -0400

    Add files via upload

---
+++ flask-blogly p1/models.py
@@ -0,0 +1,23 @@
+"""Models for Blogly."""
+
+# models.py
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    image_url = db.Column(
+        db.String, default="https://media.istockphoto.com/id/587805156/vector/profile-picture-vector-illustration.jpg?s=612x612&w=0&k=20&c=gkvLDCgsHH-8HeQe7JsjhlOY6vRBJk_sKW9lyaLgmLo=")
+
+
+def connect_db(app):
+    """Connect this database to a Flask app."""
+    db.app = app
+    db.init_app(app)
