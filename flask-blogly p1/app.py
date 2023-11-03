a72238e3d6efcd4fca966bb9267a02536589f20c
WallyRakin <83788022+WallyRakin@users.noreply.github.com>
2023-11-03 02:56:45 -0400

    Update app.py

--- flask-blogly p1/app.py
+++ flask-blogly p1/app.py
@@ -1,8 +1,9 @@
 # app.py
 
+from sqlalchemy.exc import IntegrityError
 from flask import Flask, request, render_template, redirect
 # from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post, Tag, PostTag
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://test:pass@localhost/blogly'
@@ -19,6 +20,7 @@ connect_db(app)
 
 if __name__ == '__main__':
     with app.app_context():
+        db.drop_all()
         db.create_all()
 
 
@@ -73,6 +75,52 @@ def edit_user(user_id):
         return redirect(f'/users/{user_id}')
 
     return render_template('edit.html', user=user)
+
+
+# ... (other imports and configurations)
+
+@app.route('/users/<int:user_id>/posts/new', methods=['GET', 'POST'])
+def new_post(user_id):
+    """Add a new post for the user."""
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        post = Post(title=title, content=content, user_id=user_id)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(f"/users/{user_id}")
+
+    return render_template('new_post.html', user_id=user_id)
+
+
+@app.route('/posts/<int:post_id>')
+def post_detail(post_id):
+    """Show post detail."""
+    post = Post.query.get_or_404(post_id)
+    return render_template('post_detail.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=['GET', 'POST'])
+def edit_post(post_id):
+    """Edit a post."""
+    post = Post.query.get_or_404(post_id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect(f"/posts/{post_id}")
+
+    return render_template('edit_post.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    """Delete a post."""
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(f"/users/{post.user_id}")
+
 
 
 if __name__ == '__main__':
