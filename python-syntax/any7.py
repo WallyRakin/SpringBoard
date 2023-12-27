90de4f08c2473c18da496baaa1e8c9c7dda88df6
WallyRakin <83788022+WallyRakin@users.noreply.github.com>
2023-12-27 11:29:15 -0500

    Add files via upload

--- python-syntax/any7.py
+++ python-syntax/any7.py
@@ -1,8 +1,13 @@
 def any7(nums):
     """Are any of these numbers a 7? (True/False)"""
 
-    # YOUR CODE HERE 
+    # Iterate through the list and check if any element is equal to 7
+    for num in nums:
+        if num == 7:
+            return True
+
+    # If no element is equal to 7, return False
+    return False
 
 print("should be true", any7([1, 2, 7, 4, 5]))
 print("should be false", any7([1, 2, 4, 5]))
-
