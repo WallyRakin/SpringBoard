9091286c50f074cd3af25ed8f680bca266370bff
WallyRakin <83788022+WallyRakin@users.noreply.github.com>
2023-12-31 01:20:50 -0500

    Update app.js
    removed excess comments.

--- express-middleware/app.js
+++ express-middleware/app.js
@@ -5,49 +5,4 @@ const router = require('./router');
 
 app.use('/', router);
 
-// app.use(express.json());
-
-// const items = require('./fakeDb');
-
-// app.route('/items')
-//     .get((req, res) => {
-//         res.json(items);
-//     })
-//     .post((req, res) => {
-//         const newItem = { name: req.body.name, price: req.body.price };
-//         items.push(newItem);
-//         res.status(201).json({ added: newItem });
-//     });
-
-// app.route('/items/:name')
-//     .get((req, res) => {
-//         const foundItem = items.find(item => item.name === req.params.name);
-//         if (foundItem) {
-//             res.json(foundItem);
-//         } else {
-//             res.status(404).send('Item not found');
-//         }
-//     })
-//     .patch((req, res) => {
-//         const foundItem = items.find(item => item.name === req.params.name);
-//         if (foundItem) {
-//             foundItem.name = req.body.name || foundItem.name;
-//             foundItem.price = req.body.price || foundItem.price;
-//             res.json({ updated: foundItem });
-//         } else {
-//             res.status(404).send('Item not found');
-//         }
-//     })
-//     .delete((req, res) => {
-//         const itemIndex = items.findIndex(item => item.name === req.params.name);
-//         if (itemIndex > -1) {
-//             items.splice(itemIndex, 1);
-//             res.json({ message: "Deleted" });
-//         } else {
-//             res.status(404).send('Item not found');
-//         }
-//     });
-
-
-
 module.exports = app;
