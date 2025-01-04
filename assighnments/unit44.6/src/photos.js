c67cdffa4271f64678dbe774b2d3b78aa1306708
WallyRakin <83788022+WallyRakin@users.noreply.github.com>
2025-01-04 12:54:37 -0500

    Add files via upload

---
+++ assighnments/unit44.6/src/photos.js
@@ -0,0 +1,20 @@
+import image1 from "./image1.jpg";
+import image2 from "./image2.jpg";
+import image3 from "./image3.jpg";
+
+const photos = [
+  {
+    src: image1,
+    caption: "Mrs. Puff"
+  },
+  {
+    src: image2,
+    caption: "Sea urchin"
+  },
+  {
+    src: image3,
+    caption: "Gary is that you?"
+  }
+];
+
+export default photos;
